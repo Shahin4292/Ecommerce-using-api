bd9c724812adc3ab7c04a761fe35d300e4e4a39a
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-22 13:32:40 +0600

    getX controller add complete

--- lib/controller/geX_controller.dart
+++ lib/controller/geX_controller.dart
@@ -1,16 +1,24 @@
 import 'package:api_eco/model/item_model.dart';
+import 'package:api_eco/services/service.dart';
 import 'package:get/get.dart';
 
-class ProductController extends GetxController{
-
+class ProductController extends GetxController {
   var productItem = <Product>[].obs;
 
   @override
   void onInit() {
+    fetchProduct();
     super.onInit();
   }
 
   void fetchProduct() async {
-
+    try {
+      var products = await RemoteService.fetchProduct();
+      if (products != null) {
+        productItem.assignAll(products);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
